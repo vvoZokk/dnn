8d24cc5f8327f9163f133856276333a825e044ef
Alexey <alexey.kozov@gmail.com>
2016-03-13 23:26:31 +0300

    small changes

--- dnn_project/dnn/reinforcements/input_classifier.h
+++ dnn_project/dnn/reinforcements/input_classifier.h
@@ -12,19 +12,19 @@ struct InputClassifierC : public Serializable<Protos::InputClassifierC> {
     InputClassifierC()
     : ltp(1.0)
     , ltd(-1.0)
-    , ltn(-1.0)
+    , reaction_to_noise(-1.0)
     {
     }
 
     void serial_process() {
         begin() << "ltp: " << ltp << ", "
                 << "ltd: " << ltd << ", "
-                << "ltn: " << ltn << Self::end;
+                << "reaction_to_noise: " << reaction_to_noise << Self::end;
     }
 
     double ltp;
     double ltd;
-    double ltn;
+    double reaction_to_noise;
 };
 
 
@@ -40,7 +40,8 @@ public:
                     GlobalCtx::inst().propagateReward(c.ltd);
                 }
             } else {
-                GlobalCtx::inst().propagateReward(c.ltn);
+                // unlabeled sample
+                GlobalCtx::inst().propagateReward(c.reaction_to_noise);
             }
         }
     }
